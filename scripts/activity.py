acb8125b1a6d74954fe0f8b6251ba0b17924a642
valentin druelle <valentin.druelle@unibas.ch>
2020-07-02 16:51:51 +0200

    function to create trajectory list works !

--- scripts/activity.py
+++ scripts/activity.py
@@ -1,11 +1,10 @@
-import trajectories
-import tools
-import filenames
 import numpy as np
 import copy
 import matplotlib.pyplot as plt
+import filenames
 from hivevo.patients import Patient
 from tools import get_mutation_positions, get_fixation_positions, initial_idx_mask
+from trajectories import Trajectory, create_trajectory_list
 
 
 def get_rising_positions(patient, region, aft, threshold=0.25):
@@ -47,56 +46,4 @@ aft = patient.get_allele_frequency_trajectories(region)
 threshold_low = 0.01
 threshold_high = 0.99
 
-trajectories = []
-# Exctract the full time series of af for mutations and place them in a 2D matrix as columns
-mutation_positions = tools.get_mutation_positions(patient, region, aft, threshold_low)
-region_mut_pos = np.sum(mutation_positions, axis=0, dtype=bool)
-mask = np.tile(region_mut_pos, (12, 1, 1))
-mut_frequencies = aft[mask]
-mut_frequencies = np.reshape(mut_frequencies, (12, -1))  # each column is a different mutation
-# Removing "mutation" at first time point because we don't know their history, ie rising or falling
-mut_frequencies = np.delete(mut_frequencies, np.where(mut_frequencies[0, :] > threshold_low), axis=1)
-
-filter1 = mut_frequencies > threshold_low
-filter2 = mut_frequencies < threshold_high
-
-# true for the rest of time points once it hits fixation
-filter_fixation = np.cumsum(~filter2, axis=0, dtype=bool)
-trajectory_filter = np.logical_and(~filter_fixation, filter1)
-new_trajectory_filter = np.logical_and(~trajectory_filter[:-1, :], trajectory_filter[1:, :])
-new_trajectory_filter = np.insert(new_trajectory_filter, 0, trajectory_filter[0, :], axis=0)
-trajectory_stop_filter = np.logical_and(trajectory_filter[:-1, :], ~trajectory_filter[1:, :])
-trajectory_stop_filter = np.insert(trajectory_stop_filter, 0, np.zeros(
-    trajectory_stop_filter.shape[1], dtype=bool), axis=0)
-
-# for ii in range(mut_frequencies.shape[1]):
-ii = 17
-traj = 0
-date = patient.dsi[0]
-time = patient.dsi - date
-# iterate for all trajectories inside this column
-for jj, idx_start in enumerate(np.where(new_trajectory_filter[:, ii] == True)[0]):
-
-    if not True in (trajectory_stop_filter[idx_start:, ii] == True):  # still active
-        idx_end = None
-    else:
-        idx_end = np.where(trajectory_stop_filter[:, ii] == True)[0][jj]  # fixed or lost
-
-    if idx_end == None:
-        freqs = mut_frequencies[idx_start:, ii]
-        t = time[idx_start:]
-    else:
-        freqs = mut_frequencies[idx_start:idx_end, ii]
-        t = time[idx_start:idx_end]
-
-    if idx_end == None:
-        fixation = "active"
-    elif filter_fixation[idx_end, ii] == True:
-        fixation = "fixed"
-    else:
-        fixation = "lost"
-
-    traj = trajectories.Trajectory(freqs, t, date, fixation, threshold_low, threshold_high, patient, region,
-                                   position=None, nucleotide=None)  # TODO: setup position and nucleotide
-    trajectories = trajectories + [traj]
-    breakpoint()
+trajectories = create_trajectory_list(patient, region, aft)
--- scripts/trajectories.py
+++ scripts/trajectories.py
@@ -19,17 +19,74 @@ class Trajectory():
         self.position = position                # Position on the region (int)
         self.nucleotide = nucleotide            # Nucleotide number according to HIVEVO_access/hivevo/sequence alpha
 
+    def __repr__(self):
+        return str(self.__dict__)
+
 
 def create_trajectory_list(patient, region, aft, threshold_low=0.01, threshold_high=0.99):
     trajectories = []
-
     # Exctract the full time series of af for mutations and place them in a 2D matrix as columns
-    region_mut_pos = np.sum(np.array(tools.get_mutation_positions(
-        patient, region, aft, threshold_low)), axis=0, dtype=bool)
-    mask = np.tile(region_mut_pos, (12, 1, 1))
-    mut_frequencies = np.array(aft[mask])
-    mut_frequencies = np.reshape(mut_frequencies, (12, -1))  # each column is a different mutation
+    mutation_positions = tools.get_mutation_positions(patient, region, aft, threshold_low)
+    region_mut_pos = np.sum(mutation_positions, axis=0, dtype=bool)
+    mask = np.tile(region_mut_pos, (aft.shape[0], 1, 1))
+    mut_frequencies = aft[mask]
+    mut_frequencies = np.reshape(mut_frequencies, (aft.shape[0], -1))  # each column is a different mutation
 
-    filter = np.logical_and(mut_frequencies > threshold_low, mut_frequencies < threshold_high)
+    # Map the original position and nucleotide
+    i_idx, j_idx = np.meshgrid(range(mutation_positions.shape[1]), range(
+        mutation_positions.shape[2]), indexing="ij")
+    coordinates = np.array([i_idx, j_idx])
+    coordinates = np.tile(coordinates, (aft.shape[0], 1, 1, 1))
+    coordinates = np.swapaxes(coordinates, 1, 3)
+    coordinates = np.swapaxes(coordinates, 1, 2)
+    coordinates = coordinates[mask]
+    coordinates = np.reshape(coordinates, (aft.shape[0], -1, 2))
+    # coordinates[t,ii,:] gives the [nucleotide, genome_position] of the mut_frequencies[t,ii] for any t
 
-    breakpoint()
+    # Removing "mutation" at first time point because we don't know their history, ie rising or falling
+    mask2 = np.where(mut_frequencies[0, :] > threshold_low)
+    mut_frequencies = np.delete(mut_frequencies, mask2, axis=1)
+    coordinates = np.delete(coordinates, mask2, axis=1)
+
+    filter1 = mut_frequencies > threshold_low
+    filter2 = mut_frequencies < threshold_high
+
+    # true for the rest of time points once it hits fixation
+    filter_fixation = np.cumsum(~filter2, axis=0, dtype=bool)
+    trajectory_filter = np.logical_and(~filter_fixation, filter1)
+    new_trajectory_filter = np.logical_and(~trajectory_filter[:-1, :], trajectory_filter[1:, :])
+    new_trajectory_filter = np.insert(new_trajectory_filter, 0, trajectory_filter[0, :], axis=0)
+    trajectory_stop_filter = np.logical_and(trajectory_filter[:-1, :], ~trajectory_filter[1:, :])
+    trajectory_stop_filter = np.insert(trajectory_stop_filter, 0, np.zeros(
+        trajectory_stop_filter.shape[1], dtype=bool), axis=0)
+
+    date = patient.dsi[0]
+    time = patient.dsi - date
+    # iterate though all columns (<=> mutations trajectories)
+    for ii in range(mut_frequencies.shape[1]):
+        # iterate for all trajectories inside this column
+        for jj, idx_start in enumerate(np.where(new_trajectory_filter[:, ii] == True)[0]):
+
+            if not True in (trajectory_stop_filter[idx_start:, ii] == True):  # still active
+                idx_end = None
+            else:
+                idx_end = np.where(trajectory_stop_filter[:, ii] == True)[0][jj]  # fixed or lost
+
+            if idx_end == None:
+                freqs = mut_frequencies[idx_start:, ii]
+                t = time[idx_start:]
+            else:
+                freqs = mut_frequencies[idx_start:idx_end, ii]
+                t = time[idx_start:idx_end]
+
+            if idx_end == None:
+                fixation = "active"
+            elif filter_fixation[idx_end, ii] == True:
+                fixation = "fixed"
+            else:
+                fixation = "lost"
+
+            traj = Trajectory(np.array(freqs), t, date, fixation, threshold_low, threshold_high, patient.name, region,
+                              position=coordinates[0, ii, 1], nucleotide=coordinates[0, ii, 0])
+            trajectories = trajectories + [traj]
+    return trajectories
