b2f9551dbdecfe8e585826b5d90f4479fd8e9abb
valentin druelle <valentin.druelle@unibas.ch>
2020-07-03 17:33:00 +0200

    starting to compute activity, not finished yet

--- scripts/activity.py
+++ scripts/activity.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import filenames
 from hivevo.patients import Patient
-from tools import get_mutation_positions, get_fixation_positions, initial_idx_mask
 from trajectory import Trajectory, create_trajectory_list, filter
+
 
 def get_activity(trajectory_list, time_bins):
     return None
@@ -16,7 +16,12 @@ region = "env"
 aft = patient.get_allele_frequency_trajectories(region)
 trajectories = create_trajectory_list(patient, region, aft)
 
-filtered_traj = filter(trajectories, "np.sum(traj.frequencies > 0.25, dtype=bool)")
-filtered_traj = filter(filtered_traj, "traj.t[-1] > 500")
+filtered_traj = [traj for traj in trajectories if np.sum(traj.frequencies > 0.2, dtype=bool)]
+# filtered_traj = [traj for traj in trajectories if traj.t[-1] > 0]  # Remove 1 point only trajectories
 time_bins = np.linspace(0, 1000, 10)
-get_activity(filtered_traj, time_bins)
+
+fixed, lost, active = [], [], []
+
+for ii in range(len(time_bins)-1):
+    nb_traj = len([traj for traj in trajectories if traj.t[-1] >= time_bins[ii]])
+    # TODO : design the function to get the number of active fixed and lost at a given time
