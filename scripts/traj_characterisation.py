f0dc3922ef5c7d3082dad6081eb7e63fd632a371
valentin druelle <valentin.druelle@unibas.ch>
2020-07-20 15:47:33 +0200

    Created the get_onuniform_bin functions. Not working super well

--- scripts/traj_characterisation.py
+++ scripts/traj_characterisation.py
@@ -15,8 +15,8 @@ def get_duration_distribution(trajectory_list):
     return time_bins, nb_traj
 
 
-def get_freq_distribution(trajectories, nb_bin=10):
-    frequency_bins = np.linspace(0.01, 0.99, nb_bin)
+def get_freq_distribution(trajectories, nb_bin=10, bin_range=[0.01, 0.99]):
+    frequency_bins = np.linspace(bin_range[0], bin_range[1], nb_bin)
     nb_in = []
     for ii in range(len(frequency_bins) - 1):
         nb = len([traj for traj in trajectories if np.sum(np.logical_and(
@@ -50,9 +50,41 @@ def plot_freq_ditribution(frequency_bins, nb_in, fontsize=16):
     plt.plot(frequency_bins, nb_in, '.-')
     plt.xlabel("Frequency bin center", fontsize=16)
     plt.ylabel("# Trajectories", fontsize=16)
-    plt.yscale("log")
+    # plt.yscale("log")
     plt.xlim([0, 1])
     plt.grid()
+
+
+def fit_distribution(frequency_bins, nb_in):
+    "Return an exponential fit of the distribution of nb of frequency in each bin"
+
+    def f(x, alpha, beta, gamma):
+        return alpha * np.exp(-beta * x) + gamma
+
+    import scipy
+    frequency_bins = 0.5 * (frequency_bins[:-1] + frequency_bins[1:])
+    fit = scipy.optimize.curve_fit(f, frequency_bins, nb_in, p0=[nb_in[0], 1, nb_in[-1]])
+    return fit[0]
+
+
+def get_nonuniform_bins(nb_bins, fit_params=None, bin_range=[0.05, 0.95]):
+    "Return the non_uniform bin edges that try to keep the number of trajectories in each bin constant"
+    def f(x, alpha, beta, gamma):
+        return alpha * np.exp(-beta * x) + gamma
+
+    if fit_params == None:
+        fit_params = [1418.92030293, 13.19671385, 85.01982428] # comes from fit of the distribution over 40 bins
+
+    x = np.linspace(bin_range[0], bin_range[1], nb_bins + 1)
+    x = 0.5 * (x[1:] + x[:-1])
+
+    y = f(x, *fit_params)
+    y /= np.sum(y)  # at this point y is the "density" of trajectories
+    y = 1 / y
+    y = np.cumsum(y)
+    non_uniform_bins = y / y[-1] * (bin_range[-1] - bin_range[0]) + bin_range[0]
+    non_uniform_bins = np.concatenate(([bin_range[0]], non_uniform_bins))
+    return non_uniform_bins
 
 
 if __name__ == "__main__":
@@ -77,7 +109,16 @@ if __name__ == "__main__":
     #     plot_length_distribution(filtered_traj, freq_min)
 
     ### Number of trajectory seen in frequency bin ###
-    frequency_bins, nb_in = get_freq_distribution(trajectories, nb_bin=11)
-    plot_freq_ditribution(frequency_bins, nb_in)
+    for nb_bin in [10, 20, 30]:
+        non_uniform_bins = get_nonuniform_bins(nb_bin)
+        nb_in = []
+        for ii in range(len(non_uniform_bins) - 1):
+            nb = len([traj for traj in trajectories if np.sum(np.logical_and(
+                traj.frequencies >= non_uniform_bins[ii], traj.frequencies < non_uniform_bins[ii + 1]), dtype=bool)])
+            nb_in = nb_in + [nb]
 
+        non_uniform_bins = 0.5 * (non_uniform_bins[:-1] + non_uniform_bins[1:])
+
+        plt.plot(non_uniform_bins, nb_in, label=f"{nb_bin} bins")
+    plt.legend()
     plt.show()
