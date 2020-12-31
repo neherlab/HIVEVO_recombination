97687043a635dc516d9a7638c5c8d1940a90819a
vdruelle <valentin.druelle@unibas.ch>
2020-12-31 10:15:18 +0100

    finished the make_divergence_dict function.

--- scripts/divergence.py
+++ scripts/divergence.py
@@ -44,7 +44,10 @@ def divergence_matrix(aft):
     return div
 
 
-def make_divergence_dict():
+def make_divergence_dict(time_average):
+    """
+    Creates a dictionary with the divergence in time for all patients and regions.
+    """
     regions = ["env", "pol", "gag"]
     patient_names = ["p1", "p2", "p3", "p4", "p5", "p6", "p8", "p9", "p11"]
     ref = HIVreference(subtype="any")
@@ -73,18 +76,42 @@ def make_divergence_dict():
             divergence_dict[region][patient_name]["all"] = np.array(mean_div)
             divergence_dict[region][patient_name]["dsi"] = np.array(patient.dsi)
 
+    # Computation of divergence average over all patients using interpolation
+    for region in regions:
+        time = time_average
+        divergence_dict[region]["all"] = {}
+        for mut_type in ["rev", "non_rev", "all"]:
+            nb_traj = np.zeros_like(time)
+            average_divergence = np.zeros_like(time, dtype=float)
+            for patient_name in patient_names:
+                tmp_time = time[time < divergence_dict[region][patient_name]["dsi"][-1]]
+                tmp_divergence = np.interp(
+                    tmp_time, divergence_dict[region][patient_name]["dsi"], divergence_dict[region][patient_name][mut_type])
+                average_divergence[:len(tmp_divergence)] += tmp_divergence
+                nb_traj[:len(tmp_divergence)] += 1
+
+            divergence_dict[region]["all"][mut_type] = average_divergence / nb_traj
+
     return divergence_dict
 
 
 if __name__ == "__main__":
     colors = ["C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"]
+    patient_names = ["p1", "p2", "p3", "p4", "p5", "p6", "p8", "p9", "p11"]
+    time_average = np.arange(0, 3100, 100)
 
-    divergence_dict = make_divergence_dict()
+    divergence_dict = make_divergence_dict(time_average)
 
-    # plt.plot([0], [0], 'k-', label="Reversion")
-    # plt.plot([0], [0], 'k--', label="Non-reversion")
-    # plt.grid()
-    # plt.xlabel("Time since infection [days]")
-    # plt.ylabel("Divergence")
-    # plt.legend()
-    # plt.show()
+    plt.figure()
+    for ii, region in enumerate(["env","pol","gag"]):
+        plt.plot(time_average, divergence_dict[region]["all"]["rev"], '-', color=colors[ii], label=region)
+        plt.plot(time_average, divergence_dict[region]["all"]["non_rev"], '--', color=colors[ii])
+        plt.plot(time_average, divergence_dict[region]["all"]["all"], ':', color=colors[ii])
+    plt.plot([0], [0], 'k-', label="Reversion")
+    plt.plot([0], [0], 'k--', label="Non-reversion")
+    plt.plot([0], [0], 'k:', label="All")
+    plt.grid()
+    plt.xlabel("Time since infection [days]")
+    plt.ylabel("Divergence")
+    plt.legend()
+    plt.show()
